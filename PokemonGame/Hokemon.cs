fad6e9b1314d975b46b5af6b2ea324abe7e975f9
gri202001594 <gri202001594@longroad.ac.uk>
2021-01-14 10:29:38 +0000

    Add project files.

---
+++ PokemonGame/Hokemon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonGame
+{
+    class Hokemon
+    {
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+
+        //properties are a combination of a field and method
+        // and allows us to perform checks on what is inputs
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                health = value;
+
+                // health property is keeping of health
+            }
+        }
+        public Hokemon()
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+        }
+        public void provide_name()
+        {
+            Console.WriteLine("Enter a name for your Hokemon");
+            name = Console.ReadLine();
+        }
+        public void get_details()
+        {
+            Console.WriteLine("\n\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefense: {5}", Name, Health, max_health, attack, speed, defense);
+        }
+        public int random_int_generator(int min_value, int max_value)
+        {
+            Random rnd = new Random();
+            int randomValue;
+            randomValue = rnd.Next(min_value, max_value);
+            return randomValue;
+        }
+    }
+}
+
+        
---
+++ PokemonGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PokemonGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of Hokemon!");
+            Hokemon HokeObject01 = new Hokemon();
+            HokeObject01.get_details();
+            System.Threading.Thread.Sleep(1000);//sleepsfor1second
+            //create a second hokemon
+            Hokemon HokeObject02 = new Hokemon();
+            HokeObject02.get_details();
+        }
+    }
+}
