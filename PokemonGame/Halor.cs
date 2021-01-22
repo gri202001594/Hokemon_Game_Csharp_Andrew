5ff89c30d0f9c5fbe090b8a6de26b62c881448a0
gri202001594 <gri202001594@longroad.ac.uk>
2021-01-22 14:47:25 +0000

    updated

--- PokemonGame/Halor.cs
+++ PokemonGame/Halor.cs
@@ -9,7 +9,7 @@ namespace PokemonGame
         private string team = "Halor";
         public void about() //using polymorphism to change method inherited from hokemon parent class
         {
-            Console.WriteLine("I am {0}, part of team {1}!", Name, team);
+            Console.WriteLine("I am {0}, part of team {1}!!", Name, team);
         }
     }
 }
